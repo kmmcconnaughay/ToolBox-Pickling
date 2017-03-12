ae9aea30a717efbf41563879f834ecfdf009ab69
Kerry McConnaughay <kmmcconnaughay@gmail.com>
2017-03-12 00:27:13 -0500

    Turning in ToolBox-Pickling

--- counter.py
+++ counter.py
@@ -1,5 +1,5 @@
 """ A program that stores and updates a counter using a Python pickle file"""
-
+import os
 from os.path import exists
 import sys
 from pickle import dump, load
@@ -30,7 +30,28 @@ def update_counter(file_name, reset=False):
     >>> update_counter('blah2.txt')
     2
     """
-    pass
+    file_exists = os.path.exists(file_name)
+    # when the file exists
+    if file_exists:
+        if reset == False:
+            # open file in reading plus with counter not reset
+            new_file = open(file_name, 'rb+')
+            counter = load(new_file) + 1
+            new_file.close()
+            f = open(file_name, 'wb')
+            # dump writes a pickled version of counter to the file, f
+            dump(counter, f)
+            return counter
+        # counter has been reset, so file is opened in writing mode
+        else:
+            f = open(file_name, 'wb')
+            dump(1, f)
+            return 1
+    # when the file doesn't exist, file is opened in writing mode
+    else:
+        f = open(file_name, 'wb')
+        dump(1, f)
+        return 1
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
